a4a1dfa78f06e1b78ae129f2f4f9ada3ac84f41c
Alisa <aliceversina@gmail.com>
2025-06-02 13:47:04 +0200

    Update lang.js

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,15 +1,11 @@
-```javascript
 document.addEventListener('DOMContentLoaded', () => {
   const userLang = navigator.language || navigator.userLanguage;
   const currentPath = window.location.pathname;
-  if (!currentPath.includes('/ru') && !currentPath.includes('/en') && !currentPath.includes('/de')) {
-    if (userLang.startsWith('en')) {
-      window.location.href = '/epd/en/';
+  if (currentPath === '/epd/' || currentPath === '/epd/index.html') {
+    if (userLang.startsWith('ru')) {
+      window.location.href = '/epd/ru/index.html';
     } else if (userLang.startsWith('de')) {
-      window.location.href = '/epd/de/';
-    } else {
-      window.location.href = '/epd/ru/';
+      window.location.href = '/epd/de/index.html';
     }
   }
 });
-```
